26829892dd53e52a6abb7d93f4d5ff6955e6cf2a
RickDortland <rick.dortland@gmail.com>
2026-01-28 13:30:03 +0100

    login

--- loging.php
+++ loging.php
@@ -5,41 +5,48 @@ include 'kringloop_centrum_duurzaam/includes/header.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $stmt = $pdo->prepare("SELECT * FROM user WHERE gebruikersnaam = ?");
+    $stmt = $pdo->prepare("SELECT * FROM gebruiker WHERE gebruikersnaam = ?");
     $stmt->execute([trim($_POST['gebruikersnaam'])]);
-    $user = $stmt-;>fetch(PDO::FETCH_ASSOC)
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify(trim($_POST['wachtwoord']), $user['wachtwoord'])) {
 
         $_SESSION['gebruiker'] = $user;
 
-      
-        if ($user['gebruiker'] == directie) {
-            header("Location: kringloop_centrum_duurzaam/admin/admintaak.php");
-        } 
-        if ($user['gebruiker'] == magazijnmedewerker){
-            header("Location: kringloop_centrum_duurzaam/users/magazijnmedewerker/magazijnmedewerker.php")
+        switch ($user['rollen']) {
+            case 'directie':
+                header("Location: kringloop_centrum_duurzaam/admin/admintaak.php");
+                break;
+
+            case 'magazijnmedewerker':
+                header("Location: kringloop_centrum_duurzaam/users/magazijnmedewerker/magazijnmedewerker.php");
+                break;
+
+            case 'winkelpersoneel':
+                header("Location: kringloop_centrum_duurzaam/users/winkelpersoneel/winkelpersoneel.php");
+                break;
+
+            case 'chauffeur':
+                header("Location: kringloop_centrum_duurzaam/users/chauffeur/chauffeur.php");
+                break;
+
+            default:
+                $error = "Onbekende gebruikersrol";
         }
-        if ($user['gebruiker'] == winkerpersoneel){
-            header("Location: kringloop_centrum_duurzaam/users/winkerpersoneel/winkerpersoneel.php")
-        }
-        if ($user['gebruiker'] == chauffuer){
-            header("Location: kringloop_centrum_duurzaam/users/chauffuer/chauffuer.php")
-        }
+
         exit;
 
     } else {
         $error = "Gebruikersnaam of wachtwoord onjuist";
     }
 }
-
 ?>
 
 <form method="post">
     <h2>Inloggen</h2>
 
-    <input type="text" name="username" placeholder="Gebruikersnaam" required>
-    <input type="password" name="password" placeholder="Wachtwoord" required>
+    <input type="text" name="gebruikersnaam" placeholder="Gebruikersnaam" required>
+    <input type="password" name="wachtwoord" placeholder="Wachtwoord" required>
 
     <button type="submit">Login</button>
 
